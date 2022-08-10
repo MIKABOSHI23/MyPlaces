9b304168a3ab7ad18f0fbd4759cdc0f35ab7d376
MIKABOSHI23 <a3714152241b@gmail.com>
2022-08-10 05:28:47 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var lacationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+}
